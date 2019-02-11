07f966db0c859ae651639a69d96390780aa5b5c6
Fabian Brash <fabianbrash@gmail.com>
2019-02-11 10:52:56 -0500

    initial commit

---
+++ Static-operators.ps1
@@ -0,0 +1,14 @@
+<#
+ REF:  https://docs.microsoft.com/en-us/powershell/module/microsoft.powershell.core/about/about_operators?view=powershell-6 
+ #>
+
+
+Clear-Host
+
+[datetime]::Now
+
+Write-Host "------------------------------------------------------------------------------------------------------------------------------"
+
+[datetime]::UtcNow
+Write-Host "------------------------------------------------------------------------------------------------------------------------------"
+[datetime]::Today
