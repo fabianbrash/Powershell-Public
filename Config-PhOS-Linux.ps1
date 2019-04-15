2ac950e3229bde86b30297aabe6a66eb57b8f392
Fabian Brash <fabianbrash@gmail.com>
2019-04-15 13:30:46 -0400

    changed logic to incorporate variables

--- Config-PhOS-Linux.ps1
+++ Config-PhOS-Linux.ps1
@@ -17,6 +17,10 @@ Set-PowerCLIConfiguration -WebOperationTimeoutSeconds 45000 -Confirm:$false | Ou
 
 $vc = "vc"
 
+$IP = "192.168.1.2/24"
+$DNS = "8.8.8.8"
+$GW = "192.168.1.1"
+$Domain = "photon.local"
 
 $vcCred = Import-Clixml -Path "C:\la.Cred"
 if($vcCred -eq '') {
@@ -39,19 +43,25 @@ $VM = Get-VM -Name (Read-Host "Enter VM Name") #Replace this string with your VM
 
 <# It seems you can't use empty newlines in linux#>
 
-$code3 = @'
+$code3 = @"
 cp /etc/systemd/network/99-dhcp-en.network /etc/systemd/network/99-dhcp-en.network.BAK
 sed -i 's/DHCP=yes/DHCP=no/g' /etc/systemd/network/99-dhcp-en.network
 cp -p /etc/systemd/network/99-dhcp-en.network /etc/systemd/network/10-static-en.network
-echo 'Address=192.168.1.10/24' >> /etc/systemd/network/10-static-en.network
-echo 'Gateway=192.168.1.1' >> /etc/systemd/network/10-static-en.network
-echo 'DNS=8.8.8.8' >> /etc/systemd/network/10-static-en.network
-echo 'Domains=photon.local' >> /etc/systemd/network/10-static-en.network
+echo Address=$IP >> /etc/systemd/network/10-static-en.network
+echo Gateway=$GW >> /etc/systemd/network/10-static-en.network
+echo DNS=$DNS >> /etc/systemd/network/10-static-en.network
+echo Domains=$Domain >> /etc/systemd/network/10-static-en.network
 systemctl restart systemd-networkd
 systemctl enable docker
 systemctl start docker
-tdnf check-update
-tdnf update -y >> /updates.log
+cat /etc/systemd/network/10-static-en.network
+systemctl status docker
+
+"@
+
+
+<#tdnf check-update
+tdnf update -y >> /updates.log#>
 
 '@
 
