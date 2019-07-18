ce907d18730d47dab90152365ce81908b4bc7af9
Fabian Brash <fabianbrash@gmail.com>
2019-07-18 11:05:36 -0400

    added get-esxcli logic

--- GetHostPSP.ps1
+++ GetHostPSP.ps1
@@ -20,12 +20,36 @@ Set-PowerCLIConfiguration -DefaultVIServerMode Multiple -Confirm:$false | Out-Nu
 Set-PowerCLIConfiguration -WebOperationTimeoutSeconds 45000 -Confirm:$false | Out-Null
 
 $vc = "vc"
+$PSP=@()
 
 
 Connect-VIServer -Server $vc
 
 $theHosts = Get-Datacenter -Name "dc1" | Get-VMHost
 
-$PSP = Get-ScsiLun -VmHost $theHosts -LunType disk | Select-Object -Property CanonicalName, LunType, Model, Vendor, MultipathPolicy, VMHost | ft
+$i=0
+$theHosts | % {
 
-$PSP
+    $esxcli = Get-EsxCli -VMHost $_ -V2
+
+    $PSP +=$esxcli.storage.nmp.device.list.Invoke() | Select-Object -Property Device, DeviceDisplayName, PathSelectionPolicy, StorageArrayType, @{n='VMHost';e={($theHosts[$i])}}
+
+    $i++
+}
+
+
+ $PSP | Export-Csv -Path "C:\hosts-PSP.csv" -NoTypeInformation
+
+#$PSP
+
+
+#$esxcli = Get-EsxCli -VMHost $theHost -V2
+
+#$esxcli.storage.nmp.device.list.Invoke() | Select-Object -Property Device, DeviceDisplayName, PathSelectionPolicy, StorageArrayType, @{n='VMHost';e={($theHost)}} | Export-Csv -Path "C:\hostPSP.csv" -NoTypeInformation
+
+#$esxcli.storage.nmp.device.list.Invoke() | Select-Object -Property Device, DeviceDisplayName, PathSelectionPolicy, StorageArrayType, WorkingPaths, @{n='VMHost';e={($theHost)}}
+
+
+#$PSP = Get-ScsiLun -VmHost $theHosts -LunType disk | Select-Object -Property CanonicalName, LunType, Model, Vendor, MultipathPolicy, VMHost | ft
+
+#$PSP
