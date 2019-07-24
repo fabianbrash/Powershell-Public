980db99a1f1d4dd725e3cc78906ef67a46d456d5
Fabian Brash <fabianbrash@gmail.com>
2019-07-24 15:27:24 -0400

    added MAC

--- GetNetworkAdapter.ps1
+++ GetNetworkAdapter.ps1
@@ -1,3 +1,3 @@
 Clear-Host
 
-Get-NetIPAddress | Select-Object -Property ifIndex, IPAddress, PrefixLength, PrefixOrigin, SuffixOrigin, AddressState, Store, @{n='Name';e={($_ | Get-NetAdapter | Select-Object -ExpandProperty Name)}}, @{n='InterfaceDescription';e={($_ | Get-NetAdapter | Select-Object -ExpandProperty ifDesc)}} | ft -Autosize
+Get-NetIPAddress | Select-Object -Property ifIndex, IPAddress, PrefixLength, PrefixOrigin, SuffixOrigin, AddressState, Store, @{n='Name';e={($_ | Get-NetAdapter | Select-Object -ExpandProperty Name)}}, @{n='InterfaceDescription';e={($_ | Get-NetAdapter | Select-Object -ExpandProperty ifDesc)}}, @{n='InterfaceDescription';e={($_ | Get-NetAdapter | Select-Object -ExpandProperty ifDesc)}}, @{n='MacAddress';e={($_ | Get-NetAdapter | Select-Object -ExpandProperty MacAddress)}} | ft -Autosize
