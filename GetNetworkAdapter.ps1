105bf91527ab4800cfc8fd21b78a9ff2e89e4966
Fabian Brash <fabianbrash@gmail.com>
2019-07-25 12:34:59 -0400

    complete code re-write

--- GetNetworkAdapter.ps1
+++ GetNetworkAdapter.ps1
@@ -1,3 +1,22 @@
 Clear-Host
 
-Get-NetIPAddress | Select-Object -Property ifIndex, IPAddress, SuffixOrigin, @{n='Name';e={($_ | Get-NetAdapter | Select-Object -ExpandProperty Name)}}, @{n='InterfaceDescription';e={($_ | Get-NetAdapter | Select-Object -ExpandProperty ifDesc)}}, @{n='MacAddress';e={($_ | Get-NetAdapter | Select-Object -ExpandProperty MacAddress)}}, @{n='Status';e={($_ | Get-NetAdapter | Select-Object -ExpandProperty Status)}}, @{n='LinkSpeed';e={($_ | Get-NetAdapter | Select-Object -ExpandProperty LinkSpeed)}} | ft -Autosize
+$TheHosts = @("1", "2", "3")
+$OutArray=@()
+
+$Script = {
+Get-NetIPAddress | Select-Object -Property @{n='HostName';e={($env:computername)}},IfIndex,IPAddress,SuffixOrigin ,@{n='Name';e={($_ | Get-NetAdapter | Select-Object -ExpandProperty Name)}}, @{n='InterfaceDescription';e={($_ | Get-NetAdapter | Select-Object -ExpandProperty ifDesc)}}, @{n='MacAddress';e={($_ | Get-NetAdapter | Select-Object -ExpandProperty MacAddress)}}, @{n='Status';e={($_ | Get-NetAdapter | Select-Object -ExpandProperty Status)}}, @{n='LinkSpeed';e={($_ | Get-NetAdapter | Select-Object -ExpandProperty LinkSpeed)}} | Where-Object {$_.SuffixOrigin -ne 'WellKnown'}
+
+}
+
+#$TheHost = "3"
+
+$TheHosts | % {
+
+$OutArray+=Invoke-Command -ComputerName $_ -ScriptBlock $Script -ErrorAction SilentlyContinue
+  
+ }
+
+$OutArray | Export-Csv -Path C:\Users\FABIAN4-DSA\Desktop\Output\getnet.csv -NoTypeInformation
+#$OutArray
+
+ #Get-NetIPAddress | Select-Object -Property IfIndex,IPAddress,SuffixOrigin, @{n='Name';e={($_ | Get-NetAdapter | Select-Object -ExpandProperty Name)}}, @{n='InterfaceDescription';e={($_ | Get-NetAdapter | Select-Object -ExpandProperty ifDesc)}}, @{n='MacAddress';e={($_ | Get-NetAdapter | Select-Object -ExpandProperty MacAddress)}}, @{n='Status';e={($_ | Get-NetAdapter | Select-Object -ExpandProperty Status)}}, @{n='LinkSpeed';e={($_ | Get-NetAdapter | Select-Object -ExpandProperty LinkSpeed)}} | Where-Object {$_.SuffixOrigin -ne 'WellKnown'} | ft -AutoSize
