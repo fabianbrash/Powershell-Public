63431684ec01c9c6e84be1cfc4c8d441731c217f
Fabian Brash <fabianbrash@gmail.com>
2019-08-14 09:27:38 -0400

    Update workingWithObjects.ps1

--- workingWithObjects.ps1
+++ workingWithObjects.ps1
@@ -1,4 +1,20 @@
+<#
+
+##Useful alias' with Powershell, note these are bad for legibility 
+
+
+CommandType     Name                                               Version    Source
+-----------     ----                                               -------    ------
+Alias           % -> ForEach-Object
+Alias           ? -> Where-Object
+Alias           h -> Get-History
+Alias           r -> Invoke-History
+
+#>
+
+
+
 Clear-Host
 
 
- Get-PSDrive | ? {$_.Free -gt 1} | Select-Object -Property Root, @{n='Free (GB)';e={"{0:N2}" -f ($_.Free/1gb)}}
+Get-PSDrive | ? {$_.Free -gt 1} | Select-Object -Property Root, @{n='Free (GB)';e={"{0:N2}" -f ($_.Free/1gb)}}
