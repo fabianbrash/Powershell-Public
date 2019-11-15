03995c882bb7c77d58694a2d4b029a2459b5e767
Fabian Brash <fabianbrash@gmail.com>
2019-11-15 11:32:40 -0500

    Update outgridview.ps1

--- outgridview.ps1
+++ outgridview.ps1
@@ -7,10 +7,6 @@ $data1 = Read-Host "Enter columns 1 data"
 $data2 = Read-Host "Enter column 2 data"
 $data3 = Read-Host "Enterr column 3 data"
 
-$final+=$data1
-$final+=$data2
-$final+=$data3
-
 $data = [PSCustomObject]@{
 
     Name = $data1
