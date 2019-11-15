bf3a1101a2c394b119ac9914654a733c38ae2edd
Fabian Brash <fabianbrash@gmail.com>
2019-11-15 12:10:26 -0500

    logic change

--- outgridview.ps1
+++ outgridview.ps1
@@ -1,11 +1,15 @@
 Clear-Host
 
 
+$name=@()
+$cpu=@()
+$minutes=@()
 $final=@()
 
-$data1 = Read-Host "Enter columns 1 data"
+<#$data1 = Read-Host "Enter columns 1 data"
 $data2 = Read-Host "Enter column 2 data"
 $data3 = Read-Host "Enterr column 3 data"
+
 
 $data = [PSCustomObject]@{
 
@@ -17,3 +21,31 @@ $data = [PSCustomObject]@{
 }
 
 $data | Out-GridView
+
+
+#>
+
+
+
+####Array of data######################
+
+$data4 = Read-Host "Enter names comma separated"
+$data5 = Read-Host "Enter CPU comma separated"
+$data6 = Read-Host "Enter minutes comma separated"
+
+
+$name=$data4.Split(',').Trim()
+$cpu = $data5.Split(',').Trim()
+$minutes=$data6.Split(',').Trim()
+
+  for($c=0; $c -lt $name.Length; $c++) {
+    $final+= [PSCustomObject]@{
+  
+        Name =                                $name[$c]
+        "CPU Usage %" =                       $cpu[$c]
+        "Running Time(minutes)" =             $minutes[$c]
+      }
+
+   }
+
+$final | Out-GridView
