1af3b7b7b96cdae7a3c1f70375269e967e10a43f
Fabian Brash <fabianbrash@gmail.com>
2020-01-03 13:40:48 -0500

    Update Install-DockerMsft.ps1

--- Install-DockerMsft.ps1
+++ Install-DockerMsft.ps1
@@ -3,6 +3,8 @@ Clear-Host
 
 Install-WindowsFeature -Name Containers
 
+#Let's set PSGallery as a trusted repository
+Set-PSRepository -Name "PSGallery" -InstallationPolicy Trusted
 
 Install-Module -Name DockerMsftProvider -Repository PSGallery -Force -Confirm:$false
 Install-Package -Name docker -ProviderName DockerMsftProvider -Force -Confirm:$false
