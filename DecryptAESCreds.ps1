73d2083f17c2a3a0782e265f3f595115ef622a5a
Fabian Brash <fabianbrash@gmail.com>
2020-02-04 11:10:07 -0500

    initial commit

---
+++ DecryptAESCreds.ps1
@@ -0,0 +1,49 @@
+Clear-Host
+
+function DecryptPass {
+
+
+    <#
+    
+    .SYNOPSIS
+    Decrypt a password that was encrypted using an AES-256 key
+
+    .PARAMETER ThePasswordFile
+    Path to our password file
+    
+    .PARAMETER KeyFile
+    The name and the path of our key file
+
+    .PARAMETER TheUser
+    The name of our user
+
+
+    .EXAMPLE
+
+    PS> DecryptPass -ThePasswordFile "C:\Creds\encrpass.txt" -KeyFile "C:\Creds\passkey.key" -TheUser "userA"
+    
+    #>
+
+    param(
+        [Parameter(Mandatory=$true)]
+        [ValidateNotNullOrEmpty()]
+        [String]
+        $ThePasswordFile,
+        [Parameter(Mandatory=$true)]
+        [ValidateNotNullOrEmpty()]
+        [String]
+        $KeyFile,
+        [Parameter(Mandatory=$true)]
+        [ValidateNotNullOrEmpty()]
+        [String]
+        $TheUser
+    )
+
+    $password = Get-Content $ThePasswordFile | ConvertTo-SecureString -Key (Get-Content $KeyFile)
+    $creds = New-Object System.Management.Automation.PSCredential($TheUser,$password)
+
+    <# Output our creds, obviously this is useless you would use this via -Credential $creds#>
+    $creds
+}
+
+DecryptPass -ThePasswordFile "C:\Creds\encrpass.txt" -KeyFile "C:\Creds\passkey.key" -TheUser "Administrator@vphere.local"
