21bc3e4f8196251402eb552b37cb048bd994eb1e
Fabian Brash <fabianbrash@gmail.com>
2020-02-29 13:09:30 -0500

    code rebase

--- Deploy_VM_CL.ps1
+++ Deploy_VM_CL.ps1
@@ -20,9 +20,9 @@ Set-PowerCLIConfiguration -InvalidCertificateAction Ignore -confirm:$false
 <## Note the above "fix" did not work for me what did work was setting -ErrorAction SilentlyContinue##>
 
 <# VARS #>
-$vc = "vcsa"
-$Host = "host"
-$VMName = "pctw16automate"
+$vc = "tdctlvcsa01.alexander.io"
+$theHost = "lbhpesxi01.alexander.io"
+$VMName = @("ubuntu-0", "ubuntu-1", "ubuntu-2", "ubuntu-3")
 $folder = "Staging"
 $sPG = "VMTraffic50"
 
@@ -32,26 +32,33 @@ Connect-VIServer -Server $vc
  -Location
  #>
 <# -Location IS VERY VERY IMPORTANT AS THE DATASTORE IS VISIBLE TO MULTIPLE VCENTERS AND CLUSTERS#>
-$Datastore = Get-Datasore -Name "datastore1" -Location "DC1"
+$Datastore = Get-Datastore -Name "HP-NVME" -Location "LDC"
 
-$Content_Library_Item = "18-10-Srv16-4K-EFI-OVA-VBS"
-$GuestSpec = Get-OSCustomizationSpec -Name "CustomSpec-ContentLibrary-automation"
+$Content_Library_Item = "ubuntu1804template-1"
+#$GuestSpec = Get-OSCustomizationSpec -Name "CustomSpec-ContentLibrary-automation"
 
 
 $sVMData = @{
 
     location = $folder
     Datastore = $Datastore
-    VMHost = $Host
+    VMHost = $theHost
+    DiskStorageFormat = "Thin"
  }
 
-try{
-     Get-ContentLibraryItem -Name $Content_Library_Item | New-VM -Name $VMName @sVMData -ErrorAction SilentlyContinue
-     Set-VM -VM $VMName -OSCustomizationSpec $GuestSpec -confirm:$false
-     Get-NetworkAdapter -VM $VMName | Set-NetworkAdapter -NetworkName $sPG -StartConnected $true -confirm:$false
-     Start-VM $VMName -confirm:$false
-}
-catch{
-    Write-Error -Message "Error Deploying VM from Content Library"
-}
+
+ foreach($VM in $VMName) {
+
+    try{
+        Get-ContentLibraryItem -Name $Content_Library_Item | New-VM -Name $VM @sVMData -ErrorAction SilentlyContinue
+        #Set-VM -VM $VMName -OSCustomizationSpec $GuestSpec -confirm:$false
+        Get-NetworkAdapter -VM $VM | Set-NetworkAdapter -NetworkName $sPG -StartConnected $true -confirm:$false
+        
+   }
+   catch{
+       Write-Error -Message "Error Deploying $VM from Content Library"
+   }
+   Start-VM -VM $VM -confirm:$false ##why
+ }
+
 
