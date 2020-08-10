b7cec779bbbeca4e46728317bb7e0534a92915bb
Fabian Brash <fabianbrash@gmail.com>
2020-08-10 12:04:19 -0400

    added literal paths

--- Install-Net48.ps1
+++ Install-Net48.ps1
@@ -5,10 +5,10 @@ $ProgressPreference = 'SilentlyContinue'
 
 Write-Output "[TASK 1] Downloading file(s)"
 <# This assumes Microsoft doesn't change this link #>
-Invoke-WebRequest -URI https://go.microsoft.com/fwlink/?linkid=2088631 -OutFile net48.exe
+Invoke-WebRequest -URI https://go.microsoft.com/fwlink/?linkid=2088631 -OutFile C:\net48.exe
 
 Write-Output "[TASK 2] Installing .NET 4.8.x"
 Write-Output "NOTE THE MACHINE WILL REBOOT AUTOMATICALLY..."
 Start-Sleep -Seconds 5
 
-Start-Process net48.exe -ArgumentList "/q" -Wait -Verb Runas
+Start-Process C:\net48.exe -ArgumentList "/q" -Wait -Verb Runas
