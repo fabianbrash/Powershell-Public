8bf430b122f5b211c509966e3f614b857c781356
Fabian Brash <fabianbrash@gmail.com>
2020-08-31 17:05:36 -0400

    added VBS and HW 14 check and enable

--- Deploy_VM_CL.ps1
+++ Deploy_VM_CL.ps1
@@ -60,6 +60,35 @@ $sVMData = @{
    catch{
        Write-Error -Message "Error Deploying $VM from Content Library"
    }
+   
+    ###SET VBS note the VM must be on hardware version 14
+
+ $VMHWVersion = Get-VM -Name $theVM | Select -ExpandProperty Version
+
+ if(-not($VMHWVersion -eq "v14") ) {
+     
+     Write-Host "WRONG HW VERSION MUST BE ON HW 14....SETTING NOW..." -ForegroundColor Cyan
+     #Throw "WRONG VERSION MUST BE ON HW 14"
+	 Set-VM -VM $vm -Version v14 -confirm:$false <#Deprecated method#>
+     <#Set-VM -VM $vm -HardwareVersion v14 -confirm:$false#> <#Preferred method#>
+ }
+
+ <# Note because "NestedHVEnabled" is apart of VirtualMachineConfigSpec we can just assign it a value
+ ## while VbsEnabled & VvtEnabled is apart of VirtualMachineFlagInfo so we have to create an object
+ ## first and then assign it a value, I need to get used to doing this more, it is very powerful
+ #>
+
+ $spec3 = New-Object VMware.Vim.VirtualMachineConfigSpec
+ $spec3.NestedHVEnabled = $true
+ $spec3.Flags = New-Object VMware.Vim.VirtualMachineFlagInfo
+ $spec3.Flags.VbsEnabled = $true
+ $spec3.Flags.VvtdEnabled = $true
+ $vm.ExtensionData.ReconfigVM($spec3)
+
+
+
+ Start-Sleep -Seconds 2
+ 
    Start-VM -VM $VM -confirm:$false ##why
  }
 
