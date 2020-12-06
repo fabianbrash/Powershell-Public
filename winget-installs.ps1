6312d1ed3e174e37c3e2691f692a20cae93dd7e4
Fabian Brash <fabianbrash@gmail.com>
2020-12-06 12:32:00 -0500

    added docker desktop

--- winget-installs.ps1
+++ winget-installs.ps1
@@ -3,7 +3,13 @@ Clear-Host
 $IDs = @("Microsoft.SQLServerManagementStudio", "Adobe.AdobeAcrobatReaderDC", "Mozilla.Firefox",`
 "Google.Chrome", "OpenJS.NodeJS", "GoLang.Go", "WinSCP.WinSCP", "Balena.Etcher",`
 "Microsoft.AzureStorageExplorer", "Microsoft.AzureCLI", "Amazon.AWSCLI", "7zip.7zip",`
-"Git.Git", "Microsoft.PowerShell", "Microsoft.WindowsTerminal", "Microsoft.VisualStudioCode")
+"Git.Git", "Microsoft.PowerShell", "Microsoft.WindowsTerminal", "Microsoft.VisualStudioCode",`
+"Docker.DockerDesktop")
+
+## Note Docker Desktop should be last as it installs a ton of features that requires a reboot, also it
+## Enables WSL2 which might require in kernel update just follow the directions after the install and
+## Then reboot, also if this is a VM make sure you enable features that expose the CPU or this will
+## Note work, i.e. nested VMs
 
 ## Let's set the code to zero so we can have a clean run
 $LASTEXITCODE = 0
