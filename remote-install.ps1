5b23a6d65441f4a47bb5ff1413ff90e3fe7fa22c
Fabian Brash <fabianbrash@gmail.com>
2020-12-23 10:57:02 -0500

    Add files via upload

---
+++ remote-install.ps1
@@ -0,0 +1,18 @@
+﻿Clear-Host
+
+
+
+Invoke-Command -ComputerName TDCQWWSUS-1.fabianbrash.com -ScriptBlock {
+
+  #$URI = "https://www.7-zip.org/a/7z1900-x64.msi"
+  $URI = "https://www.7-zip.org/a/7z1900-x64.exe"
+
+  <# Speed up downloads #>
+  $ProgressPreference = 'SilentlyContinue'
+
+  Invoke-WebRequest -URI $URI -OutFile C:\7z1900-x64.exe
+  $args = @("/S")
+  $exepath = "C:\7z1900-x64.exe"
+  Start-Process -FilePath $exepath -ArgumentList $args -Wait -Verb RunAs -PassThru
+
+}
