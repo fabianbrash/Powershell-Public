a2256d3289914887eff3bcc47e58f0bd9c996e2e
Fabian Brash <fabianbrash@gmail.com>
2021-02-21 19:34:00 -0500

    Update Set-BGInfo-reg-key.ps1

--- Set-BGInfo-reg-key.ps1
+++ Set-BGInfo-reg-key.ps1
@@ -1,6 +1,8 @@
 Clear-Host
 
 
-$rootKey = 'HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Run'
-$bginfo = 'C:\BGInfo\Bginfo.exe C:\BGInfo\default.bgi /timer:0 /silent /nolicprompt'
+$rootKey = "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Run"
+$bginfo = "C:\BGInfo\Bginfo.exe C:\BGInfo\default.bgi /timer:0 /silent /nolicprompt"
+
+Set-ItemProperty -Path $rootKey -Name "BgInfo" -Type String -Value $bginfo
 
