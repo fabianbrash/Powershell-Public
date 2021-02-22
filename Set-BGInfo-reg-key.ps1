80f48b33f38cfab636ce5cd9f6dec3e07980b22c
Fabian Brash <fabianbrash@gmail.com>
2021-02-22 08:56:49 -0500

    added azure blob download and unzip

--- Set-BGInfo-reg-key.ps1
+++ Set-BGInfo-reg-key.ps1
@@ -1,5 +1,13 @@
 Clear-Host
 
+<# Let's speed up our downloads.. #>
+$ProgressPreference = 'SilentlyContinue' 
+
+<# Let's download our zip file #>
+Invoke-WebRequest -URI https://containerblobs.blob.core.windows.net/installers/BGInfo.zip -OutFile C:\BGInfo.zip
+
+<# Expand our zip file #>
+Expand-Archive -Path C:\BGInfo.zip -DestinationPath C:\
 
 $rootKey = "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Run"
 $bginfo = "C:\BGInfo\Bginfo.exe C:\BGInfo\default.bgi /timer:0 /silent /nolicprompt"
