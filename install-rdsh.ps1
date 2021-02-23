a2d3896059bdf50234b8afb25a8e83d488f6423c
Fabian Brash <fabianbrash@gmail.com>
2021-02-23 15:09:57 -0500

    added reg logic

--- install-rdsh.ps1
+++ install-rdsh.ps1
@@ -1,3 +1,14 @@
 Clear-Host
 
-Install-WindowsFeature -Name "Remote-Desktop-Services", "RDS-RD-Server" -IncludeManagementTools -Restart
+Install-WindowsFeature -Name "Remote-Desktop-Services", "RDS-RD-Server" -IncludeManagementTools
+
+<# Let's add our reg keys #>
+
+$KEY = "HKLM:\SOFTWARE\Policies\Microsoft\Windows NT\Terminal Services"
+$licenseServer = "my_server"
+$licenseMode = 4  <# 4=per user 2=per device #>
+
+New-ItemProperty -Path $KEY -Name "LicenseServers" -PropertyType String -Value $licenseServer
+New-ItemProperty -Path $KEY -Name "LicenseMode" -PropertyType DWord -Value $licenseMode
+
+Restart-Computer -Confirm:$false
