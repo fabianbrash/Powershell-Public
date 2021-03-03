c48508cf157599f1f3f3243d8367938a3c7ab355
Fabian Brash <fabianbrash@gmail.com>
2021-03-03 12:48:43 -0500

    initial commit

---
+++ Get-Scheduled-TasksV3.ps1
@@ -0,0 +1,21 @@
+Clear-Host
+
+$s = New-PSSession -ComputerName CompA,CompB,CompC
+
+Invoke-Command -Session $s {
+
+ $Data=@()
+ 
+ $Tasks = Get-ScheduledTask | Where-Object {$_.TaskPath -like "\"} #Root path only
+ 
+ foreach($taskName in $Tasks.TaskName) {
+ 
+   $Data+= Get-ScheduledTaskInfo -TaskName $taskName | Select-Object -Property TaskName, LastRunTime, NextRunTime, NumberOfMissedRuns, LastTaskResult
+ 
+ }
+ 
+ $Data
+
+} | Export-Csv -Path C:\tasks.csv -NotypeInformation -Append
+
+# | Out-File -FilePath C:\tasks.log -Width 10000 -Append
