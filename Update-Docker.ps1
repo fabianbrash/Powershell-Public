a6ed1e65163b55ce911b403b1642b352799c3050
Fabian Brash <fabianbrash@gmail.com>
2021-04-24 09:29:23 -0400

    initial commit

---
+++ Update-Docker.ps1
@@ -0,0 +1,18 @@
+<#
+  Purpose: Update docker version
+#>
+
+Clear-Host
+
+
+Find-Package -Name Docker -ProviderName DockerMSFTProvider
+
+
+Install-Package -Name Docker -ProviderName DockerMSFTProvider -Update -Force
+
+
+Start-Sleep -Seconds 10
+
+Restart-Service docker
+
+Restart-Compute -Confirm:$false
