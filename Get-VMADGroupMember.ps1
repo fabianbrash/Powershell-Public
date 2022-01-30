1e55e3bab7dfd31fa847fcae8bf503747d5893c3
Fabian Brash <fabianbrash@gmail.com>
2022-01-30 10:22:55 -0500

    Update Get-VMADGroupMember.ps1

--- Get-VMADGroupMember.ps1
+++ Get-VMADGroupMember.ps1
@@ -17,47 +17,54 @@ Set-PowerCLIConfiguration -WebOperationTimeoutSeconds 45000 -Scope User -Confirm
 
 ####Ignore Certificate issues this resolves an issue with New-VM in PowerCLI 11.0 but I'm certain for peace of mind just do this by default
 
-Set-PowerCLIConfiguration -InvalidCertificateAction Ignore -confirm:$false | Out-Null
+Set-PowerCLIConfiguration -InvalidCertificateAction Ignore -Scope User -confirm:$false | Out-Null
+
+Set-PowerCLIConfiguration -Scope User -ParticipateInCEIP $false -Confirm:$false | Out-Null
 
 $vc = "vc"
 
 
-Connect-VIServer -Server $vc  -Credential $vcCred
+Connect-VIServer -Server $vc -Credential $vcCred
 
 
 $VM = Get-VM -Name (Read-Host "Enter VM Name") #Replace this string with your VM name
 
 #$VMList = @()
 $VMList = Get-Content -Path C:\path_to_text_file\file.txt
-$theGroup = "MyGroup"
+
 
 $VMs = Get-VM -Name $VMList
 
-$code3 = @"
-$found = Get-LocalGroupMember -
-Name 'Administrators' | where{$_.Name -like '*'+$theGroup}
+$code3 = @'
+$theGroup = "MyGroup"
+$found = Get-LocalGroupMember -Name 'Administrators' | Where-Object { $_.Name -like '*'+$theGroup }
 if($found) {
-    Write-Host "$theGroup found on $env:COMPUTERNAME"
+    Write-Host "Found group $theGroup"
 }
 else {
-    Write-Host "Did not find $theGroup"
+    Write-Host "Did not find group $theGroup"
 }
+'@
+
+$code2 = @"
+Get-Process
 "@
 
+## Note the difference above it seems if you want to use variables you need @' '@ if you don't need variables then @" "@ is okay
 <# Get creds for OS #>
-$cred = Get-Credential
+$guestCredentials = Get-Credential
 
 $VMs | ForEach-Object {
 
     $sINvoke = @{
       VM = $_
-      GuestCredential = $cred
+      GuestCredential = $guestCredentials
       ToolsWaitSecs = 120
       ScriptText = $code3
-      ScriptType = PowerShell
+      
     }
 
-
+#ScriptType = PowerShell note this caused the script to just stop and do nothing...
     Invoke-VMScript @sINvoke
 
 }
