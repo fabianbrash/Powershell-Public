07b20845b4822c1d763cfe5957796b0d23b3f5e4
Fabian Brash <fabianbrash@gmail.com>
2022-02-25 09:17:47 -0500

    Create Get-File-Share-permissions.ps1

---
+++ Get-File-Share-permissions.ps1
@@ -0,0 +1,16 @@
+#REF:https://www.lepide.com/how-to/get-an-ntfs-permissions-report-using-powershell.html
+Clear-Host
+
+$thePath_or_Share = "C:\temp"
+
+
+$FolderPath = Get-ChildItem -Directory -Path $thePath_or_Share -Recurse -Force
+$Output = @()
+ForEach ($Folder in $FolderPath) {
+    $Acl = Get-Acl -Path $Folder.FullName
+    ForEach ($Access in $Acl.Access) {
+$Properties = [ordered]@{'Folder Name'=$Folder.FullName;'Group/User'=$Access.IdentityReference;'Permissions'=$Access.FileSystemRights;'Inherited'=$Access.IsInherited}
+$Output += New-Object -TypeName PSObject -Property $Properties            
+}
+}
+$Output | Out-GridView
