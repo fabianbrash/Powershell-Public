b50164612136dd04f009330b9aea33caa02b3ef2
Fabian Brash <fabianbrash@gmail.com>
2023-10-18 08:58:14 -0400

    Update Setup-Server2022.ps1

--- Setup-Server2022.ps1
+++ Setup-Server2022.ps1
@@ -21,62 +21,76 @@ $arg7 = 'winget install --id Microsoft.WindowsTerminal.Preview --source winget -
 $arg8 = 'winget install --id Microsoft.PowerShell --source winget --accept-package-agreements --accept-source-agreements'
 $arg9 = 'winget install --id Postman.Postman --source winget --accept-package-agreements --accept-source-agreements'
 
-## Install chocolatey
 
-Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+function installchoco {
 
+    ## Install chocolatey
 
-## install tanzu cli
-
-Start-Process powershell.exe -ArgumentList $arg4 -Wait
-
-## install Firefox
-
-Start-Process powershell.exe -ArgumentList $arg5 -Wait
+    Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+}
 
 
-## We need to download these packages
+function downloadpayloads {
 
-Invoke-WebRequest -URI https://github.com/microsoft/winget-cli/releases/download/v1.7.2782-preview/Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle -OutFile $profile$folder"Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle"
+    ## We need to download these packages
+
+    Invoke-WebRequest -URI https://github.com/microsoft/winget-cli/releases/download/v1.7.2782-preview/Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle -OutFile $profile$folder"Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle"
+
+    iwr -URI https://github.com/microsoft/winget-cli/releases/download/v1.7.2782-preview/f1c7c505b9934655be2195c074913cbf_License1.xml -OutFile $profile$folder"f1c7c505b9934655be2195c074913cbf_License1.xml"
+
+    iwr -URI https://www.nuget.org/api/v2/package/Microsoft.UI.Xaml/2.7.3 -OutFile $profile$folder"microsoft.ui.xaml.2.7.3.nupkg"
+
+    ## We need to rename this so we can unzip it
+
+    Rename-Item -Path $profile$folder"microsoft.ui.xaml.2.7.3.nupkg" -NewName $profile$folder"microsoft.ui.xaml.2.7.3.zip"
+
+    Expand-Archive -Path $profile$folder"microsoft.ui.xaml.2.7.3.zip" -DestinationPath $profile$folder
+
+    iwr -URI https://aka.ms/Microsoft.VCLibs.x64.14.00.Desktop.appx -OutFile $profile$folder"Microsoft.VCLibs.x64.14.00.Desktop.appx"
+
+}
+
+function installpackages {
+
+    ## install tanzu cli
+
+    Start-Process powershell.exe -ArgumentList $arg4 -Wait
+
+    ## install Firefox
+
+    Start-Process powershell.exe -ArgumentList $arg5 -Wait
+
+    ## Try to install winget
+
+    Add-AppxPackage -Path $profile$folder"tools\AppX\x64\Release\Microsoft.UI.Xaml.2.7.appx"
+
+    Add-AppxPackage -Path $profile$folder"Microsoft.VCLibs.x64.14.00.Desktop.appx"
+
+    Add-AppxProvisionedPackage -Online -PackagePath $profile$folder"Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle" -LicensePath $profile$folder"f1c7c505b9934655be2195c074913cbf_License1.xml" -Verbose
+
+    ## If winget is installed successfully these are some packages that would be nice to have
+
+    Start-Process powershell.exe -ArgumentList $arg1 -Verb RunAs -Wait
+
+    Start-Process powershell.exe -ArgumentList $arg2 -Verb RunAs -Wait
+
+    Start-Process powershell.exe -ArgumentList $arg3 -Verb RunAs -Wait
+
+    Start-Process powershell.exe -ArgumentList $arg6 -Verb RunAs -Wait
+
+    Start-Process powershell.exe -ArgumentList $arg7 -Verb RunAs -Wait
+
+    Start-Process powershell.exe -ArgumentList $arg8 -Verb RunAs -Wait
+
+    Start-Process powershell.exe -ArgumentList $arg9 -Verb RunAs -Wait
+
+}
+
+
+installchoco
+downloadpayloads
+installpackages
 
 
 
-iwr -URI https://github.com/microsoft/winget-cli/releases/download/v1.7.2782-preview/f1c7c505b9934655be2195c074913cbf_License1.xml -OutFile $profile$folder"f1c7c505b9934655be2195c074913cbf_License1.xml"
 
-
-iwr -URI https://www.nuget.org/api/v2/package/Microsoft.UI.Xaml/2.7.3 -OutFile $profile$folder"microsoft.ui.xaml.2.7.3.nupkg"
-
-## We need to rename this so we can unzip it
-
-Rename-Item -Path $profile$folder"microsoft.ui.xaml.2.7.3.nupkg" -NewName $profile$folder"microsoft.ui.xaml.2.7.3.zip"
-
-Expand-Archive -Path $profile$folder"microsoft.ui.xaml.2.7.3.zip" -DestinationPath $profile$folder
-
-
-iwr -URI https://aka.ms/Microsoft.VCLibs.x64.14.00.Desktop.appx -OutFile $profile$folder"Microsoft.VCLibs.x64.14.00.Desktop.appx"
-
-## Try to install winget
-
-Add-AppxPackage -Path $profile$folder"tools\AppX\x64\Release\Microsoft.UI.Xaml.2.7.appx"
-
-Add-AppxPackage -Path $profile$folder"Microsoft.VCLibs.x64.14.00.Desktop.appx"
-
-
-Add-AppxProvisionedPackage -Online -PackagePath $profile$folder"Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle" -LicensePath $profile$folder"f1c7c505b9934655be2195c074913cbf_License1.xml" -Verbose
-
-
-## If winget is installed successfully these are some packages that would be nice to have
-
-Start-Process powershell.exe -ArgumentList $arg1 -Verb RunAs -Wait
-
-Start-Process powershell.exe -ArgumentList $arg2 -Verb RunAs -Wait
-
-Start-Process powershell.exe -ArgumentList $arg3 -Verb RunAs -Wait
-
-Start-Process powershell.exe -ArgumentList $arg6 -Verb RunAs -Wait
-
-Start-Process powershell.exe -ArgumentList $arg7 -Verb RunAs -Wait
-
-Start-Process powershell.exe -ArgumentList $arg8 -Verb RunAs -Wait
-
-Start-Process powershell.exe -ArgumentList $arg9 -Verb RunAs -Wait
