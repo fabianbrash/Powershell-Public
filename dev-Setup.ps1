30b63ac1985e7ea3f0994e63f941055459800fa4
Fabian Brash <fabianbrash@gmail.com>
2024-01-24 12:03:46 -0500

    Add files via upload

--- dev-Setup.ps1
+++ dev-Setup.ps1
@@ -12,6 +12,7 @@ $arg4 = 'choco install dotnet-sdk --yes'
 $arg6 = 'choco install nodejs-lts --yes'
 $arg7 = 'choco install golang --yes'
 $arg8 = 'choco install python --yes'
+$arg9 = 'choco install git --yes'
 
 function CheckErrorCode($exitCode, $errorMessage) {
     if ($exitCode -ne 0) {
@@ -60,6 +61,10 @@ function installpackages {
     ## install Python 3.x
     $process8 = Start-Process powershell.exe -ArgumentList $arg8 -Verb RunAs -PassThru -Wait -ErrorAction Continue
     CheckErrorCode $process8.ExitCode "Error installing Python3 3.x"
+
+    ## install Git
+    $process9 = Start-Process powershell.exe -ArgumentList $arg9 -Verb RunAs -PassThru -Wait -ErrorAction Continue
+    CheckErrorCode $process9.ExitCode "Error installing Python3 3.x"
 }
 
 $policy = Get-ExecutionPolicy
